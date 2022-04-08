86eda6b6b209169505b2336d4214c1d1f4491d6b
Daniela Jovevska <danielajovevska@hotmail.com>
2022-04-08 18:55:23 +0200

    initial commit

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,81 @@
+import Card from './shared/Card';
+import Button from './shared/Button';
+import RatingSelect from './shared/RatingSelect';
+import { useContext, useState, useEffect } from 'react';
+import FeedbackContext from './context/FeedbackContext';
+
+function FeedbackForm() {
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+  const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext);
+
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
+
+  console.log(feedbackEdit);
+
+  const handleTextChange = ({ target: { value } }) => {
+    if (value === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (value.trim().length < 10) {
+      setMessage('Text must be at least 10 characters!');
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      console.log(newFeedback);
+
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
+      }
+      setBtnDisabled(true);
+      setRating(10);
+      setText('');
+    }
+  };
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={setRating} selected={rating} />
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            value={text}
+            type='text'
+            placeholder='Write a review'
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>{' '}
+        </div>
+        <br />
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
